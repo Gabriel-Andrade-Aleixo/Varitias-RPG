20a451c45f8e3092f2f2b65aa591be749da30d11
gabriel aleixo <aleixo.gabriel.aleixo@gmail.com>
2024-07-09 17:14:32 -0300

    1.0

--- scriptArma.js
+++ scriptArma.js
@@ -1,46 +1,74 @@
 const armas = {
-    espada: {
-        nome: "Espada",
-        descricao: "Uma espada afiada feita de aço.",
+    Rifle_M1A: {
+        nome: "Rifle_M1A",
+        descricao: "Rifle M1A com cano de 16 polegadas.",
         imagem: "#", //Selecionar image.......
         dano: "1d12",
-        tipo: "Corpo a Corpo",
-        usaMana: "Não",
-        tipoDano: "Corte",
-        dadoCritico: "19-20/x2",
-        alcance: "Corpo a Corpo",
-        atkAdicional: "+2",
-        valor: "50 PO",
-        danoAtributo: "Força"
-    },
-    arco: {
-        nome: "Arco",
-        descricao: "Um arco longo com flechas de penas.",
-        imagem: "#", //Selecionar image.......
-        dano: "1d6",
-        tipo: "À Distância",
-        usaMana: "Não",
-        tipoDano: "Perfuração",
+        tipo: "Distância",
+        usaMana: "1mp",
+        tipoDano: "Físico",
         dadoCritico: "20/x3",
-        alcance: "18 metros",
-        atkAdicional: "+1",
-        valor: "30 PO",
-        danoAtributo: "Destreza"
+        alcance: "200m",
+        atkAdicional: "0",
+        valor: "50T$",
+        danoAtributo: "Pontaria"
     },
-    cajado: {
-        nome: "Cajado",
-        descricao: "Um cajado mágico com poderes místicos.",
+    Olhos_Místicos: {
+        nome: "Olhos Místicos de Tuatha Dé",
+        descricao: "Olhos místicos possados para membros da família Tuatha Dé por meio de uma cirurgia, Passada de geração para geração. Permite determinar a mana, nv e força das coisas e possoas, podendo também ver longas distâncias com facilidade, ganhando proeficiência em percepção e pontaria.",
         imagem: "#", //Selecionar image.......
-        dano: "1d4",
-        tipo: "Mágico",
-        usaMana: "Sim",
-        tipoDano: "Mágico",
+        dano: "0",
+        tipo: "Especial",
+        usaMana: "Não",
+        tipoDano: "Nenhum",
+        dadoCritico: "0",
+        alcance: "1,5km",
+        atkAdicional: "0",
+        valor: "0",
+        danoAtributo: "0"
+    },
+    Gáe_Bolg: {
+        nome: "Gáe_Bolg",
+        descricao: "Uma das armas lendárias, capaz de acabar com uma guerra inteira com um unico ataque. Quando ativada e lançada ela atira um bombardeio ao redor que causa 1d4 de dano cada bomba. A ativação desta habilidade gasta 60mp. É necessário ter uma força maior que 50 para poder erguer esta arma.",
+        imagem: "#", //Selecionar image.......
+        dano: "1d4/3d12",
+        tipo: "Mágico/Perfurante",
+        usaMana: "Sim/Não",
+        tipoDano: "Mágico/Físico",
+        dadoCritico: "18-20/x2",
+        alcance: "100m/2m",
+        atkAdicional: "0/5",
+        valor: "1.250.000T$",
+        danoAtributo: "Misticismo/Luta"
+    },
+    Foice_Grafeno: {
+        nome: "Foice de Grafeno",
+        descricao: "Uma característica especial desta arma é a resistência ao calor dela. por conta de seu material, é uma arma extremamente leve e aguenta uma temperatura de cerca de 3851° C.",
+        imagem: "#", //Selecionar image.......
+        dano: "1d8",
+        tipo: "Cortante",
+        usaMana: "Não",
+        tipoDano: "Físico",
         dadoCritico: "20/x2",
-        alcance: "9 metros",
-        atkAdicional: "+0",
-        valor: "100 PO",
-        danoAtributo: "Inteligência"
-    }
+        alcance: "2m",
+        atkAdicional: "0",
+        valor: "250T$",
+        danoAtributo: "Luta"
+    },
+    Dead_Rainbow: {
+        nome: "Dead end Rainbow",
+        descricao: "Contém atributos que a torna super eficaz contra Dragões e Demônios, fazendo todo acerto ser dano crítico",
+        imagem: "#", //Selecionar image.......
+        dano: "2d12+1d8",
+        tipo: "Cortante",
+        usaMana: "Não",
+        tipoDano: "Físico",
+        dadoCritico: "19-20/x2",
+        alcance: "2m",
+        atkAdicional: "+5",
+        valor: "1.550.000T$",
+        danoAtributo: "Luta"
+    },
 };
 
 // Função para atualizar os detalhes da arma
